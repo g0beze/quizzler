c65810099be610c998f68a8ddf64962431be6a34
MENDEX <39565143+M3ND3X@users.noreply.github.com>
2023-04-15 22:47:04 +0300

    updated the prodcedure of setState()

--- lib/main.dart
+++ lib/main.dart
@@ -46,20 +46,20 @@ class _QuizPageState extends State<QuizPage> {
   void checkAnswer(bool userPickedAnswer) {
     bool correctAnswer = quizBrain.getQuestionAnswer();
 
-    if (userPickedAnswer == correctAnswer) {
-      scoreKeeper.add(const Icon(
-        Icons.check,
-        color: Colors.green,
-      ));
-    } else {
-      scoreKeeper.add(const Icon(
-        Icons.close,
-        color: Colors.red,
-      ));
-    }
-
     setState(
       () {
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(const Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          scoreKeeper.add(const Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+
         quizBrain.nextQuestion();
       },
     );
